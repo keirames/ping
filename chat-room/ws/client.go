168ea772c2aaa6d502ce37630ef98ab85ea1bc99
khang nguyen <keirames.l3@gmail.com>
2023-06-13 23:24:00 +0700

    remove ping pong mechanism

--- chat-room/ws/client.go
+++ chat-room/ws/client.go
@@ -15,10 +15,7 @@ const (
 	writeWait = 10 * time.Second
 
 	// Time allowed to read the next pong message from the peer.
-	pongWait = 2 * time.Second
-
-	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10
+	readWait = 10 * time.Second
 
 	// Maximum message size allowed from peer.
 	maxMessageSize = 512
@@ -45,42 +42,29 @@ var upgrader = websocket.Upgrader{
 }
 
 func (c *client) writeBump() {
-	ticker := time.NewTicker(pingPeriod)
 	defer func() {
-		ticker.Stop()
 		c.conn.Close()
 	}()
 
-	for {
-		select {
-		case msg, ok := <-c.send:
-			if !ok {
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
+	msg, ok := <-c.send
 
-			w, err := c.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
-				return
-			}
-			_, err = w.Write(msg)
-			if err != nil {
-				return
-			}
+	err := c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+	if err != nil {
+		return
+	}
 
-		case <-ticker.C:
-			err := c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err != nil {
-				fmt.Println("set write deadline error", err)
-				return
-			}
+	if !ok {
+		c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+		return
+	}
 
-			err = c.conn.WriteMessage(websocket.PingMessage, nil)
-			if err != nil {
-				fmt.Println("write ping message error", err)
-				return
-			}
-		}
+	w, err := c.conn.NextWriter(websocket.TextMessage)
+	if err != nil {
+		return
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return
 	}
 }
 
@@ -91,16 +75,7 @@ func (c *client) readBump() {
 	}()
 
 	c.conn.SetReadLimit(maxMessageSize)
-	c.conn.SetReadDeadline(time.Now().Add(pongWait))
-	c.conn.SetPongHandler(
-		func(string) error {
-			err := c.conn.SetReadDeadline(time.Now().Add(pongWait))
-			if err != nil {
-				return err
-			}
-			return nil
-		},
-	)
+	c.conn.SetReadDeadline(time.Now().Add(readWait))
 
 	for {
 		_, message, err := c.conn.ReadMessage()
@@ -121,6 +96,10 @@ func (c *client) readBump() {
 }
 
 func Serve(h *hub, w http.ResponseWriter, r *http.Request) {
+	// TODO: DEV only
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		logger.L.Error().Err(err).Msg("Cannot upgrade http")
